24497fe56852c27dab33ed00835e54a96b18f7db
YieMon55 <yiemon532003@gmail.com>
2026-09-11 14:57:20 +0630

    my local edit

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 import numpy as np
 num1=int(input("Enter your number:"))
 num2=int(input("Enter your number:"))
-num = num1-num2
-print("Substraction:",num)
+num = num1+num2
+print("Additional",num)
